0094c8f1656f45fc394d41314fadb08911362fd6
karem70 <106421493+karem70@users.noreply.github.com>
2022-08-06 05:56:32 +0200

    جامع الذهب
    يجعلنى اعرف سعر قطعة الذهب من وزنه

---
+++ text.js
@@ -0,0 +1,15 @@
+
+let x =prompt();
+document.write('<h3>عيار 24 </h3>');
+document.write(x * 1111); document.write('<br>')
+document.write('<h3>عيار 21 </h3>');
+document.write(x * 972); document.write('<br>')
+document.write('<h3>عيار 18 </h3>');
+document.write(x * 833); document.write('<br>')
+document.write('<h3>عيار 14 </h3>');
+document.write(x * 648); document.write('<br>')
+document.write('<h3>عيار 10 </h3>');
+document.write(x * 463); document.write('<br>')
+
+
+
